119e1972e592eb67eca00afb4563a7dc4f0cd336
Abhishek Paul <abhishek.paulcp.dbs@gmail.com>
2020-08-24 03:33:51 +0530

    added solution to p1

---
+++ p1.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    long sum = 0;
+    for(int i = 1; i < 1000; i++) {
+        if(i % 3 == 0 || i % 5 == 0) {
+            sum += i;
+        }
+    }
+    cout<<sum;
+    return 0;
+}
